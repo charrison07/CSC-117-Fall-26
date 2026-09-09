220710ec3e3cb97015a51e12d7daa83e204a9e3c
charrison07 <cjrharrison2020@gmail.com>
2026-09-09 15:14:09 +0000

    review

---
+++ 01-output/review.py
@@ -0,0 +1,8 @@
+#review
+name = input("What's your name?")
+print (f"Hello {name}!")
+birth_year = int(input("What year were you born"))
+age = 2026 -birth_year
+print (f"cool! you are {age} years old!")
+dog_age = age * 7
+print (f"your age in dog years is {dog_age}")
---
+++ 01-output/review2.py
@@ -0,0 +1,5 @@
+#review
+print("boxes - dozen donuts")
+print(15//12) #floor division
+print("individual donuts(not in the box)")
+print(15%12) #mod operator
