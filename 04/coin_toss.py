453017bb76f92b1973a40d791e4c49861a6ae7d9
charrison07 <cjrharrison2020@gmail.com>
2026-09-23 16:06:53 +0000

    labs

---
+++ 04/coin_toss.py
@@ -0,0 +1,63 @@
+# FILE NAME - coin_toss.py
+
+# NAME: 
+# DATE: 
+# BRIEF DESCRIPTION:  
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions and Attestation below
+# 4. The Sample Output has been included in this code for your convenience
+
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+# Don't forget to import random!!!!!
+import random
+flip =random.randint(1,100)
+print("===== Coin Flipper =====")
+if flip >= 51:
+    print("Heads")
+else:
+    print("Tails")
+
+
+########### END YER CODE ABOVE THIS LINE ###########
+
+    
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+'''
+===== Coin Flipper =====
+Heads
+'''
+
+
+
+'''
+===== Coin Flipper =====
+Tails
+'''
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. What was the hardest part of completing this lab? 
+knowing where to start
+
+
+
+
+
+
+'''
